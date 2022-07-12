ea30c505049d8f009188dc4a176b0d8442d95f5e
HarlockOfficial <harlockofficial.github@gmail.com>
2022-07-12 11:38:24 +0200

    [delete] unused function, alternative to this function is already present and used

--- TEnCu - Framework/Assets/Scripts/Startup/Download/AssetBundlesManager.cs
+++ TEnCu - Framework/Assets/Scripts/Startup/Download/AssetBundlesManager.cs
@@ -1,5 +1,4 @@
-﻿using Models.Version;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Networking;
 
 namespace Startup.Download
@@ -17,21 +16,6 @@ namespace Startup.Download
 
             if (request.result == UnityWebRequest.Result.Success) 
                 return DownloadHandlerAssetBundle.GetContent(request);
-            Debug.LogError(request.error);
-            return null;
-        }
-
-        public static VersionList GetRemoteVersionList(string url)
-        {
-            var request = UnityWebRequest.Get(url);
-            request.SendWebRequest();
-            while (!request.isDone)
-            {
-                // Waiting
-            }
-
-            if (request.result == UnityWebRequest.Result.Success)
-                return JsonUtility.FromJson<VersionList>(request.downloadHandler.text);
             Debug.LogError(request.error);
             return null;
         }
